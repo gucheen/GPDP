115cdaaabdf781aa985d8a85f4864cf602b0df84
gucheng <guchengf@gmail.com>
2015-10-28 20:49:26 +0800

    update: demo

--- demo.js
+++ demo.js
@@ -1,31 +1,31 @@
 var testDp = new DP(document.getElementsByClassName('dp-container')[0]);
 
-  testDp.listen('changeValue', function (value) {
-    document.getElementsByClassName('test-result')[0].textContent = value;
-  });
+testDp.listen('changeValue', function (value) {
+  document.getElementsByClassName('test-result')[0].textContent = value;
+});
 
-  var testDp2 = new DP(document.getElementsByClassName('test2')[0], {
-    name: 'Complex data',
-    data: [{
-      label: 'Object',
-      value: {
-        a: 1
-      }
-    }, {
-      label: 'Array',
-      value: [1, 2]
-    }, {
-      label: 'Function',
-      value: function () {
-        return 'value returned by function';
-      }
-    }, {
-      label: 'Null',
-      value: null
-    }]
-  });
+var testDp2 = new DP(document.getElementsByClassName('test2')[0], {
+  name: 'Complex data',
+  data: [{
+    label: 'Object',
+    value: {
+      a: 1
+    }
+  }, {
+    label: 'Array',
+    value: [1, 2]
+  }, {
+    label: 'Function',
+    value: function () {
+      return 'value returned by function';
+    }
+  }, {
+    label: 'Null',
+    value: null
+  }]
+});
 
-  testDp2.listen('changeValue', function (value) {
-    console.log('%cLOG VALUE: testDp2', 'color: blue', value);
-    document.getElementsByClassName('test2-result')[0].textContent = (typeof value) + ' : ' + JSON.stringify(value, null, 2);
-  });
+testDp2.listen('changeValue', function (value) {
+  console.log('%cLOG VALUE: testDp2', 'color: blue', value);
+  document.getElementsByClassName('test2-result')[0].textContent = (typeof value) + ' : ' + JSON.stringify(value, null, 2);
+});
